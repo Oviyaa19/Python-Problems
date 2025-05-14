4a4961d405c50b0dd7d96acd516a2c31a7c26b41
Oviyaa19 <158291394+Oviyaa19@users.noreply.github.com>
2025-05-14 15:11:13 +0530

    Problem2

---
+++ problem2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first value: "))
+b = int(input("Enter the second value: "))
+c = int(input("Enter the third value: "))
+result = a+b+c
+
+if(result==180):
+    print("Triangle can be formed")
+else:
+    print("Triangle cannot be formed")
