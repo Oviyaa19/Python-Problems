de8a0fa29f3778c13c4c8b7e368029b30dda9983
Oviyaa19 <158291394+Oviyaa19@users.noreply.github.com>
2025-05-14 22:46:34 +0530

    Add files via upload

---
+++ problem7.py
@@ -0,0 +1,7 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+number  = int(input("Enter a number: "))
+fullName = firstName + lastName
+
+for i in range(number):
+    print(fullName)
