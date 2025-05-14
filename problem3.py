0bcf6b7786a467550065387aad76a3c0d709874f
Oviyaa19 <158291394+Oviyaa19@users.noreply.github.com>
2025-05-14 15:24:06 +0530

    Problem3

---
+++ problem3.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your marks: "))
+
+if(marks >= 90 ):
+    print("Grade A")
+elif(marks >= 80):
+    print("Grade B")
+elif(marks >= 60):
+    print("Grade C")
+elif(marks >= 35):
+    print("Grade D")
+else:
+    print("Fail")
+
