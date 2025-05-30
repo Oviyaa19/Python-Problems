2f95a96e77fdf6e95ad34233a2232f3774c7328a
Oviyaa19 <158291394+Oviyaa19@users.noreply.github.com>
2025-05-30 23:55:41 +0530

    problem15

---
+++ problem15.py
@@ -0,0 +1,16 @@
+
+n = 5
+
+for i in range(1, n):
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+    for j in range(1, 2 * i):
+        print("*", end=" ")
+    print()
+
+for i in range(1, n + 1):
+    for j in range(1, i):
+        print(" ", end=" ")
+    for j in range(1, (2 * n) - (2 * i - 1) + 1):
+        print("*", end=" ")
+    print()
